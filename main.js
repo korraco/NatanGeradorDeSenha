8e5a3603c26a011bf906a23af4895ea50e7a1010
NatanHenrique <102609229+korraco@users.noreply.github.com>
2024-04-30 16:18:13 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,77 +1,58 @@
-const numeroSenha = document.querySelector('.parametro-senha__texto');
-let tamanhoSenha = 12;
-numeroSenha.textContent = tamanhoSenha;
-const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
-const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
-const numeros = '0123456789';
-const simbolos = '!@%*?';
-const botoes = document.querySelectorAll('.parametro-senha__botao');
-const campoSenha = document.querySelector('#campo-senha');
-const checkbox = document.querySelectorAll('.checkbox');
-const forcaSenha = document.querySelector('.forca');
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 
-botoes[0].onclick = diminuiTamanho;
-botoes[1].onclick = aumentaTamanho;
+for (let i = 0; i < botoes.length; i++) {
+botoes[i].onclick = function () {
 
-function diminuiTamanho() {
-if (tamanhoSenha > 1) {
-// tamanhoSenha = tamanhoSenha-1;
-tamanhoSenha--;
-}
-numeroSenha.textContent = tamanhoSenha;
-geraSenha();
-}
-function aumentaTamanho() {
-if (tamanhoSenha < 20) {
-// tamanhoSenha = tamanhoSenha+1;
-tamanhoSenha++;
-}
-numeroSenha.textContent = tamanhoSenha;
-geraSenha();
+for (let j = 0; j < botoes.length; j++) {
+botoes[j].classList.remove("ativo");
+textos[j].classList.remove("ativo");
 }
 
-for (i = 0; i < checkbox.length; i++) {
-checkbox[i].onclick = geraSenha;
+botoes[i].classList.add("ativo");
+textos[i].classList.add("ativo");
+}
 }
 
-geraSenha();
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 
-function geraSenha() {
-let alfabeto = '';
-if (checkbox[0].checked) {
-alfabeto = alfabeto + letrasMaiusculas;
-}
-if (checkbox[1].checked) {
-alfabeto = alfabeto + letrasMinusculas;
-}
-if (checkbox[2].checked) {
-alfabeto = alfabeto + numeros;
-}
-if (checkbox[3].checked) {
-alfabeto = alfabeto + simbolos;
-}
-let senha = '';
-for (let i = 0; i < tamanhoSenha; i++) {
-let numeroAleatorio = Math.random() * alfabeto.length;
-numeroAleatorio = Math.floor(numeroAleatorio);
-senha = senha + alfabeto[numeroAleatorio];
-}
-campoSenha.value = senha;
-classificaSenha(alfabeto.length);
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
+
+function calculaTempo(tempoObjetivo) {
+let tempoAtual = new Date();
+let tempoFinal = tempoObjetivo - tempoAtual;
+let segundos = Math.floor(tempoFinal / 1000);
+let minutos = Math.floor(segundos / 60);
+let horas = Math.floor(minutos / 60);
+let dias = Math.floor(horas / 24);
+
+segundos %= 60;
+minutos %= 60;
+horas %= 24;
+if (tempoFinal > 0){
+return [dias,horas,minutos,segundos];
+} else {
+return 24,24,24,24];
+}
 }
 
-function classificaSenha(tamanhoAlfabeto){
-let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto);
-console.log(entropia);
-forcaSenha.classList.remove('fraca','media','forte');
-if (entropia > 57){
-forcaSenha.classList.add('forte');
-} else if (entropia > 35 && entropia < 57 ) {
-forcaSenha.classList.add('media');
-} else if (entropia <= 35){
-forcaSenha.classList.add('fraca');
+function atualizaCronometro(){
+for (let i=0; i<contadores.length;i++){
+document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
+document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
+document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
+document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];
 }
-const valorEntropia = document.querySelector('.entropia');
-valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
 }
+
+function comecaCronometro(){
+atualizaCronometro();
+setInterval(atualizaCronometro,1000);
+}
+
+comecaCronometro();
